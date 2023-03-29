8324f0f62bef149dac63da8845c1efbb4179cefb
Aribunfi <arianna@pabis.de>
2023-03-29 09:01:40 +0200

    prove di segnaposto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{env('APP_NAME')}}</title>
+    <title>{{ env('APP_NAME') }}</title>
     @vite('resources/js/app.js')
 </head>
 <body>
-    
+    <h1>Template title</h1>
+    <p>
+        testo: </ br>
+        @yield('main_paragraph')
+    </p>
 </body>
 </html>
