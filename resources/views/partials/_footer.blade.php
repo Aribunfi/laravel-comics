558f8e8ada364d97e0d40827a6403582b2abd014
Aribunfi <arianna@pabis.de>
2023-03-30 16:58:06 +0200

    footer

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,4 +1,23 @@
-<div class="card-footer text-muted">
-Footer 
-</div>
-</div>
+<div class="card-group">
+    <div class="card">
+      <div class="card-body">
+        <button type="button">SIGN UP NOW!</button>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title"></h5>
+        <p class="card-text"></p>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <a href="#">Follow us</a>
+        <i class="fa-brands fa-facebook"></i>
+        <i class="fa-brands fa-square-twitter"></i>
+        <i class="fa-brands fa-youtube"></i>
+        <i class="fa-brands fa-pinterest"></i>
+        <i class="fa-solid fa-location-dot"></i>
+      </div>
+    </div>
+  </div>
