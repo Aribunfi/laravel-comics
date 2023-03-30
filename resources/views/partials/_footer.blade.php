b99cac14e7faa00a125f07bc551db0cc384eb61b
Aribunfi <arianna@pabis.de>
2023-03-30 17:13:03 +0200

    correzione bottone footer

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,23 +1,25 @@
-<div class="card-group bg-dark">
-    <div class="card bg-dark">
-      <div class="card-body bg-dark">
-        <button type="button">SIGN UP NOW!</button>
+<footer class="bg-dark text-white mt-auto">
+  <div class="card-group bg-dark">
+      <div class="card bg-dark">
+        <div class="card-body bg-dark">
+          <button type="button" class="btn btn-outline-primary">SIGN UP NOW!</button>
+        </div>
+      </div>
+      <div class="card bg-dark">
+        <div class="card-body bg-dark">
+          <h5 class="card-title"></h5>
+          <p class="card-text"></p>
+        </div>
+      </div>
+      <div class="card bg-dark">
+        <div class="card-body bg-dark">
+          <a href="#">Follow us</a>
+          <img src="../resources/img/footer-facebook.png" alt="">
+          <img src="../resources/img/footer-twitter.png" alt="">
+          <img src="../resources/img/footer-youtube.png" alt="">
+          <img src="../resources/img/footer-pinterest.png" alt="">
+          <img src="../resources/img/footer-periscope.png" alt="">
+        </div>
       </div>
     </div>
-    <div class="card bg-dark">
-      <div class="card-body bg-dark">
-        <h5 class="card-title"></h5>
-        <p class="card-text"></p>
-      </div>
-    </div>
-    <div class="card bg-dark">
-      <div class="card-body bg-dark">
-        <a href="#">Follow us</a>
-        <img src="../resources/img/footer-facebook.png" alt="">
-        <img src="../resources/img/footer-twitter.png" alt="">
-        <img src="../resources/img/footer-youtube.png" alt="">
-        <img src="../resources/img/footer-pinterest.png" alt="">
-        <img src="../resources/img/footer-periscope.png" alt="">
-      </div>
-    </div>
-  </div>
+</footer>
