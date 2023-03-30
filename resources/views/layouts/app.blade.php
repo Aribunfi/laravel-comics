355c5a191e842aca6284cfceef889132e6203708
Aribunfi <arianna@pabis.de>
2023-03-30 16:17:04 +0200

    multipli tentativi di far vedere il database

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,16 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> {{ env('APP_NAME') }} </title>
+    <title>{{ env('APP_NAME') }}</title>
     @vite('resources/js/app.js')
 </head>
 <body>
-    <h1>Template title</h1>
-    <p>
-        testo: </ br>
-        @yield('main_paragraph')
-    </p>
-</body>
+    <h1>Title</h1>
+    
+    @dump($comics)
 
-@include('partials._footer');
+
+</body>
 </html>
