f1d87a990c211b733770549463a4ef954ac6957b
Aribunfi <arianna@pabis.de>
2023-03-30 16:20:18 +0200

    segnaposto con title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ env('APP_NAME') }}</title>
+    <title>{{ env('APP_NAME') }} - @yield('title')</title>
     @vite('resources/js/app.js')
 </head>
 <body>
-    <h1>Title</h1>
+    <h1>@yield('title')</h1>
     
     @dump($comics)
 
