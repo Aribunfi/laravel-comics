8ae851fd058083819ae217360fe5618880d1ff04
Aribunfi <arianna@pabis.de>
2023-03-30 17:05:23 +0200

    correzione icone social del footer

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,23 +1,23 @@
-<div class="card-group">
-    <div class="card">
-      <div class="card-body">
+<div class="card-group bg-dark">
+    <div class="card bg-dark">
+      <div class="card-body bg-dark">
         <button type="button">SIGN UP NOW!</button>
       </div>
     </div>
-    <div class="card">
-      <div class="card-body">
+    <div class="card bg-dark">
+      <div class="card-body bg-dark">
         <h5 class="card-title"></h5>
         <p class="card-text"></p>
       </div>
     </div>
-    <div class="card">
-      <div class="card-body">
+    <div class="card bg-dark">
+      <div class="card-body bg-dark">
         <a href="#">Follow us</a>
-        <i class="fa-brands fa-facebook"></i>
-        <i class="fa-brands fa-square-twitter"></i>
-        <i class="fa-brands fa-youtube"></i>
-        <i class="fa-brands fa-pinterest"></i>
-        <i class="fa-solid fa-location-dot"></i>
+        <img src="../resources/img/footer-facebook.png" alt="">
+        <img src="../resources/img/footer-twitter.png" alt="">
+        <img src="../resources/img/footer-youtube.png" alt="">
+        <img src="../resources/img/footer-pinterest.png" alt="">
+        <img src="../resources/img/footer-periscope.png" alt="">
       </div>
     </div>
   </div>
