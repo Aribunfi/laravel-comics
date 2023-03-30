9fb9cb78ab4c9cdf86709c0784e3dc04d350fc52
Aribunfi <arianna@pabis.de>
2023-03-30 16:45:56 +0200

    route alla home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,46 +9,8 @@
     @vite('resources/js/app.js')
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-          <img src="../resources/img/dc-logo.png" alt="Logo_DB_comics">
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Characters</a>
-                  </li>
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Comics</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Movies</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Tv</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">Games</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">Collectibles</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">Videos</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">Fans</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">News</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">Shops</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    {{-- @include('partials._navbar') --}}
+    
+    @include('partials._navbar')
     
     <main>
         <h1>@yield('title')</h1>
