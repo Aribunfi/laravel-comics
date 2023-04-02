314f772197de5fdb4ff78947948fb107e87b4197
Aribunfi <arianna@pabis.de>
2023-04-02 18:37:52 +0200

    aggiustate foto con picsum e abbellimenti

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -5,9 +5,18 @@ Homepage
 @endsection
 
 @section('content')
-<div class="row">
+<div class="row bg-dark justify-content-center">
     @foreach($comics as $comic);
-    <h2>{{ $comic['title'] }}</h2>
+    <div class="col-2 bg-dark">
+        <div class="card bg-dark text-white text-center" style="width: 18rem;">
+            <img src="https://picsum.photos/300/500" class="card-img-top" alt="thumb">
+            <div class="card-body d-flex flex-column">
+            <p class="card-text">{{$comic['title']}}</p>
+            </div>
+        </div>
+    </div>
     @endforeach
+    <button type="button" class="btn btn-primary">LOAD MORE</button>
 </div>
 @endsection
+
